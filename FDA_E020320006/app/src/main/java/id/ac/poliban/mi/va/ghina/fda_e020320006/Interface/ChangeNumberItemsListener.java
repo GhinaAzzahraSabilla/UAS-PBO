61977a4f9fe9aa2ae5ddfef242320997cf26b544
GhinaAzzahraSabilla <ghinasabilla11@gmail.com>
2023-01-15 17:30:26 +0800

    UAS

---
+++ FDA_E020320006/app/src/main/java/id/ac/poliban/mi/va/ghina/fda_e020320006/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.va.ghina.fda_e020320006.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ WisataApp_E020320006/app/src/main/java/id/ac/poliban/mi/ghina/wisataapp_e020320006/utils/Tools.java
@@ -0,0 +1,15 @@
+package id.ac.poliban.mi.ghina.wisataapp_e020320006.utils;
+
+import android.content.Context;
+
+public class Tools {
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
